86a028301c686ab44222cee43baeb407923fb20c
usmanoyekan <130054813+usmanoyekan@users.noreply.github.com>
2023-05-20 22:10:22 +0100

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -20,7 +20,7 @@ module "eks" {
   version = "18.21.0"
 
   cluster_name = "myapp-eks-cluster"  
-  cluster_version = "1.20"
+  cluster_version = "1.22"
 
   subnet_ids = module.myapp-vpc.private_subnets
   vpc_id = module.myapp-vpc.vpc_id
@@ -33,11 +33,11 @@ module "eks" {
   eks_managed_node_groups = {
     dev = {
       min_size     = 1
-      max_size     = 3
-      desired_size = 3
+      max_size     = 2
+      desired_size = 2
 
       instance_types = ["t2.small"]
-      key_name       = "devopskeypair"
+      key_name       = "usmanoyekanKP"
     }
   }
 }
